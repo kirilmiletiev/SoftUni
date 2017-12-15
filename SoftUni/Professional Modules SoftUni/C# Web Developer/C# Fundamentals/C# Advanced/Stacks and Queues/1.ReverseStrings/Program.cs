7288a96f5b048b1cc870ecde89b3844cba99cb17
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-15 16:33:00 +0200

    Stacks
    Stacks and Queues

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues/1.ReverseStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToCharArray();
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(stack.Pop());
+               
+            }
+            Console.WriteLine();
+        }
+    }
+}
