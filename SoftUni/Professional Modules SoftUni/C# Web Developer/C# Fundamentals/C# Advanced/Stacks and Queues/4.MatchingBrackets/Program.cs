17bb011b332bf15b7267cc95dbea14c437a656b0
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-16 17:39:24 +0200

    Matching Brackets
    C# Advanced / Stacks and Queues

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues/4.MatchingBrackets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]== '(')
+                {
+                    stack.Push(i.ToString());
+                }
+
+                if (input[i]==')')
+                {
+                    var startIndex = int.Parse(stack.Pop());
+                    var str = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
