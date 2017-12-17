5276b4b49eea77758f6fc3a4d550d3f685412f62
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-17 20:32:27 +0200

    Hot Potato
    problem  - Stacks and Queues

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues/5.HotPotato/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Queue<string> queue = new Queue<string>(input.Split(' '));
+            var num = int.Parse(Console.ReadLine());
+
+            while (queue.Count>1)
+            {
+                for (int i = 0; i < num-1; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+
+                }
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {queue.Peek()}");
+        }
+    }
+}
