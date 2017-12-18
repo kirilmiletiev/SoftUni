f19e0b1ca168a0ffc81c5f84b766bf856f49ddaa
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-18 23:40:31 +0200

    Stacks and Queues - Exercise
    SoftUni

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues - Exercise/01. Reverse Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Reverse_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            Stack<string> stack = new Stack<string>(input);
+            var n = input.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+               // Console.Write(" ");
+                Console.Write(stack.Pop());
+                Console.Write(" ");
+            }
+        }
+    }
+}
---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues - Exercise/02. Basic Stack Operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var n = int.Parse(input[0]);
+            var s = int.Parse(input[1]);
+            var x = int.Parse(input[2]);
+
+            var nums = (Console.ReadLine().Split());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                stack.Push(int.Parse(nums[i]));
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                //stack.ToArray();
+                Console.WriteLine(stack.Min());
+            }
+
+        }
+    }
+}
