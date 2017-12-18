9c4f8466905e7af6d6834b29bb534ba91949e60e
Kiril Miletiev <kirilmiletiev@gmail.com>
2017-12-18 22:58:18 +0200

    new

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues/2.SimpleCalculatorr/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.SimpleCalculatorr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<string> stack = new Stack<string>(input.Reverse());
+
+            while (stack.Count>1)
+            {
+
+                int first = int.Parse(stack.Pop());
+                string plusMinus = stack.Pop();
+                int second = int.Parse(stack.Pop());
+
+                if (plusMinus == "-")
+                {
+                    stack.Push((first-second).ToString());
+                }
+                else if(plusMinus == "+")
+                {
+                    stack.Push((first + second).ToString());
+                }
+
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues/3.DecimalToBinaryConverter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.DecimalToBinaryConverter
+{
+    class Converter     
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            while (n >0)
+            {
+                stack.Push(n%2);
+
+                n = n / 2;
+            }
+            int stacker = stack.Count;
+
+            for (int i = 0; i < stacker; i++)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
