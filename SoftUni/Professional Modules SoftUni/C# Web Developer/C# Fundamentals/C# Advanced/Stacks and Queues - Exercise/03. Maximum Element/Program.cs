0efdf128fd2d9b148899d90e6ce54a1766d76d67
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-19 23:49:31 +0200

    03. Maximum Element
    SoftUni

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues - Exercise/03. Maximum Element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Maximum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<BigInteger> stack = new Stack<BigInteger>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                var command = input[0];
+
+                if (command=="1")
+                {
+                    var num = BigInteger.Parse(input[1]);
+                    stack.Push(num);
+                }
+                else if (command == "2")
+                {
+                    stack.Pop();
+                }
+                else if (command == "3")
+                {
+                    Console.WriteLine(stack.Max());
+                }
+            }
+        }
+    }
+}
