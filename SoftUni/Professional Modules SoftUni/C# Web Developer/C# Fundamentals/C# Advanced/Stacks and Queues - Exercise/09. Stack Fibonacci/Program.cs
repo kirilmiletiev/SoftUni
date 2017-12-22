fbb1870ba674edc6bfc52f8e26f8420ce63bd79b
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-22 22:12:09 +0200

    00. Stack Fibonacci
    SoftUni C# Advanced

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues - Exercise/09. Stack Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Stack_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            var counter = 0;
+            var n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            stack.Push(0);
+            stack.Push(1);
+            for (int i = 0; i < n; i++)
+            {
+                var one = stack.Peek();
+                sum = stack.Pop() + stack.Pop();
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
