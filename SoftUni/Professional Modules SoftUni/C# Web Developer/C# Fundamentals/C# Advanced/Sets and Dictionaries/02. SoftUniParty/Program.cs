65e749a24490f08633328dbec4d7fb9cba0b71f9
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-24 23:49:28 +0200

    Sets and Dictionaries LAB
    C# Advanced SoftUni

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Sets and Dictionaries/02. SoftUniParty/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> normalSet = new HashSet<string>();
+            HashSet<string> vipSet = new HashSet<string>();
+            var input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+
+                foreach (char ch in input)
+                {
+                    if (char.IsDigit(ch))
+                    {
+                        vipSet.Add(input);
+                        break;
+                    }
+                    else
+                    {
+                        normalSet.Add(input);
+                        break;
+                    }
+                }
+                
+                input = Console.ReadLine();
+            }
+
+            var secondInput = Console.ReadLine();
+            while (secondInput!= "END")
+            {
+                if (vipSet.Contains(secondInput))
+                {
+                    vipSet.Remove(secondInput);
+                }
+
+                if (normalSet.Contains(secondInput))
+                {
+                    normalSet.Remove(secondInput);
+                }
+                secondInput = Console.ReadLine();
+            }
+
+            var nummmm = normalSet.Count + vipSet.Count;
+
+
+            Console.WriteLine($"That is the nummber of the guest, who did not come {nummmm}");
+
+            Console.WriteLine("---------------------NORMAL-----------");
+            foreach (var key in normalSet)
+            {
+                Console.WriteLine(key);
+            }
+            Console.WriteLine("---------------------VIP-----------");
+            foreach (var key in vipSet)
+            {
+
+                Console.WriteLine(key);
+            }
+        }
+    }
+}
