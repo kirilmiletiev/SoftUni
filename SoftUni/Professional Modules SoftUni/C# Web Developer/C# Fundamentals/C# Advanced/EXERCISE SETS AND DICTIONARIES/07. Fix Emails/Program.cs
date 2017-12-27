c49e5d5ce24b712d7abfbb9071efe52c7c9ec5fb
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-27 20:02:25 +0100

    EXERCISE SETS AND DICTIONARIES
    C# Advanced SoftUni

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/EXERCISE SETS AND DICTIONARIES/07. Fix Emails/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Fix_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            var name = Console.ReadLine();
+
+            while (name != "stop")
+            {
+                var email = Console.ReadLine();
+
+                if (!dict.ContainsKey(name))
+                {
+                    if (email.Contains("@") && !email.EndsWith(".us") && !email.EndsWith(".uk"))
+                    {
+                        dict.Add(name, email);
+                    }
+
+                }
+                name = Console.ReadLine();
+            }
+            foreach (KeyValuePair<string, string> keyValuePair in dict)
+            {
+                //Console.WriteLine(keyValuePair);
+                Console.WriteLine($"{keyValuePair.Key} -> {keyValuePair.Value}");
+            }
+        }
+    }
+}
