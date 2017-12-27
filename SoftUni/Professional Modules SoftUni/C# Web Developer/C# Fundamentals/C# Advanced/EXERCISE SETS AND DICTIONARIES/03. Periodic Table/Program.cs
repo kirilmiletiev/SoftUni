8a940ded6c73427204ab6d99e95cc2cc50110865
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-27 13:54:13 +0100

    SoftUni C# Advanced
    EXERCISE SETS AND DICTIONARIES

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/EXERCISE SETS AND DICTIONARIES/03. Periodic Table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>(); 
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var element = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var s in element)
+                {
+                    if (!set.Contains(s))
+                    {
+                        set.Add(s);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", set.OrderBy(x=>x)));
+        }
+    }
+}
