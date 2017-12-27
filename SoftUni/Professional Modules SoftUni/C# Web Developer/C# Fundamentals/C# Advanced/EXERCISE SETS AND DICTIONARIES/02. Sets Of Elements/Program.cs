23816057bde30999aadd4dd5b32a31201a6bf395
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2017-12-27 13:52:09 +0100

    02. Sets Of Elements
    SoftUni Advanced

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/EXERCISE SETS AND DICTIONARIES/02. Sets Of Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sets_Of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int num = int.Parse(input[0]) + int.Parse(input[1]);
+
+            for (int i = 0; i < num; i++)
+            {
+                var n = Console.ReadLine();
+
+                if (!set.Contains(n))
+                {
+                    set.Add(n);
+                }
+                else
+                {
+                    Console.Write(n + ' ');
+                }
+            }
+
+        }
+    }
+}
