1d3259326dec24946e5dc60c3b957872c3592c74
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-01-22 20:54:29 +0100

    02. Sum Numbers

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/FUNCTIONAL PROGRAMMING - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+            Console.WriteLine($"{input.Count()}\n{input.Sum()}");
+
+        }
+    }
+}
