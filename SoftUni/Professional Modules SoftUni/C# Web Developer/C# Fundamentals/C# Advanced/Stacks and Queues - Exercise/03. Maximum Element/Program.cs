d9293413bf62ca42b03e9c6ea7242544ce0ec041
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-01-24 15:48:27 +0200

    fix class

--- SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues - Exercise/03. Maximum Element/Program.cs
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Stacks and Queues - Exercise/03. Maximum Element/Program.cs
@@ -2,57 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _03.Maximum_Element
+namespace ConsoleApp1
 {
     class Program
     {
-        static Stack<int> stack = new Stack<int>();
-        private static int max = 0;
-
-        static void Main()
+        static void Main(string[] args)
         {
-            
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
-            {
-                int[] input = Console.ReadLine()
-                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
-                    .ToArray();
-                GetCommand(input);
-            }
-        }
+            long maxNum = long.MinValue;
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            Stack<long> stack = new Stack<long>();
 
-        private static void GetCommand(int[] args)
-        {
-            switch (args[0])
+            for (int i = 0; i < numberOfCommands; i++)
             {
-                case 1:
-                    stack.Push(args[1]);
-                    if (args[1] > max)
-                    {
-                        max = args[1];
-                    }
-                    break;
-                case 2:
-                    int element = stack.Pop();
-                    if (element == max && stack.Count > 0)
-                    {
-                        max = stack.Max();
-                    }
-                    else if (element == max && stack.Count == 0)
-                    {
-                        max = 0;
-                    }
-                    break;
-                case 3:
-                    Console.WriteLine(max);
-                    break;
+                var commandDate = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+                var command = (commandDate[0]);
 
-                default:
-                    break;
-                    
-                        
+                if (command == 1)
+                {
+                    stack.Push(commandDate[1]);
+                    if (commandDate[1] > maxNum)
+                    {
+                        maxNum = commandDate[1];
+                    }
+                }
+                else if (command == 2)
+                {
+                    if (stack.Peek() == maxNum)
+                    {
+                        stack.Pop();
+                        if (stack.Count>0)
+                        {
+                            maxNum = stack.Max();
+                        }
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+                if (stack.Count == 0)
+                {
+                    maxNum = long.MinValue;
+                }
+                else if (command == 3)
+                {
+                    Console.WriteLine(maxNum);
+                }
             }
         }
     }
