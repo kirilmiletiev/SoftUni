fe7d8b809ce871cec63c17809150d8e9e14e416d
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-02 20:32:26 +0100

    04. Add VAT

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/FUNCTIONAL PROGRAMMING - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new[] { ',', ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+            Console.WriteLine(string.Join("\n", input.Select((x => $"{x * 1.20:F2}"))));
+        }
+    }
+}
