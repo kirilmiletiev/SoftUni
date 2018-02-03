9e49cca807a621d90c1db386cde5d0a021ecabb2
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-03 13:41:46 +0100

    04. Find Evens or Odds

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/FUNCTIONAL PROGRAMMING -exercises/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse)
+                .ToList();
+            var oddEven = Console.ReadLine();
+            for (long i = input[0]; i <= input[1]; i++)
+            {
+                if (oddEven == "even")
+                {
+                    if (Math.Abs(i) % 2 == 0)
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+                else if (oddEven == "odd")
+                {
+                    if (Math.Abs(i) % 2 == 1)
+                    {
+                        Console.Write($"{i} ");
+                    }
+
+                }
+            }
+        }
+    }
+}
