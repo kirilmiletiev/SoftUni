1a61374f5a5c55c4b53b80ab835c0bb40c3358c2
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-03 13:38:18 +0100

    02. Knights of Honor

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/FUNCTIONAL PROGRAMMING -exercises/02. Knights of Honor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> act = x =>
+            {
+                foreach (string s in x)
+                {
+                    Console.WriteLine($"Sir {s}");
+                }
+            };
+            var input = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            act(input);
+        }
+    }
+}
