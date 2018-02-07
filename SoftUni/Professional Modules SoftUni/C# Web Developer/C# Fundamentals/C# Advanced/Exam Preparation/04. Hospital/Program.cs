73c31ccf69c0b31d28e8eef82f095861a5c8e40b
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-07 22:57:53 +0200

    Fix bug hospital

--- SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Exam Preparation/04. Hospital/Program.cs
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# Advanced/Exam Preparation/04. Hospital/Program.cs
@@ -1,107 +1,71 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-
-namespace _04._Hospital
+using System.Text;
+namespace _06.Hospital
 {
-    public class Patient
-    {
-        public string test { get; set; }
-        public string departament;
-        public string doctor;
-        public string name;
-        public int room;
-    }
-    class Program
+    class Hospital
     {
         static void Main(string[] args)
         {
-            Dictionary<string, List<string>> doctors = new Dictionary<string, List<string>>();
-            Dictionary<string, List<string>> departaments = new Dictionary<string, List<string>>();
-
-            var data = Console.ReadLine();
-            while (data != "Output")
+            var departments = new Dictionary<string, List<string>>();
+            var doctors = new Dictionary<string, List<string>>();
+            var line = Console.ReadLine();
+ 
+            while (line != "Output")
             {
-                var hospital = data.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-                var depart = hospital[0];
-                var doctor = hospital[1] + " " + hospital[2];
-                var patient = hospital[3];
-                ///add to departaments DICTIONARY
-                if (!departaments.ContainsKey(depart))
+                var tokens = line.Split().ToArray();
+                var dep = tokens[0];
+                var dfn= tokens[1]+" "+ tokens[2];
+                var patient = tokens[3];
+                if (!departments.ContainsKey(dep))
                 {
-                    departaments.Add(depart, new List<string>());
+                    departments[dep] = new List<string>();
                 }
-                departaments[depart].Add(patient);
-
-                ///add to doctors DICTIONARY
-                if (!doctors.ContainsKey(doctor))
+                departments[dep].Add(patient);
+                if (!doctors.ContainsKey(dfn))
                 {
-                    doctors.Add(doctor, new List<string>());
+                    doctors[dfn] = new List<string>();
                 }
-                doctors[doctor].Add(patient);
-
-                data = Console.ReadLine();
+                doctors[dfn].Add(patient);
+                line = Console.ReadLine();
             }
-
-            ////second part (PRINT)
-            var ъ = Console.ReadLine().Trim();
-
-            while (ъ != "End")
+            line = Console.ReadLine().Trim();
+            while (line != "End")
             {
-
-                GetPrint(departaments, doctors, ъ);
-
-                ъ = Console.ReadLine();
-            }
-
-        }
-
-        private static void GetPrint(Dictionary<string, List<string>> departaments, Dictionary<string, List<string>> doctors, string ъ)
-        {
-            var wtf = ъ.Split(' ').ToArray();
-            //int num = 0;
-            //bool isNum = false;
-            //isNum = int.TryParse(wtf[1], out num);
-
-
-
-            //if (ъ.Length == 1)
-            if(!ъ.Contains(" "))
-            {
-                foreach (string s in departaments[ъ])
+                var token = line.Split().ToArray();
+                if (token.Length == 1)
                 {
-                    Console.WriteLine(s);
-                }
-                return;
-            }
-            if ((ъ.Length > 1))
-            {
-                int num = 0;
-                bool isNum = false;
-                isNum = int.TryParse(wtf[1], out num);
-
-                if (isNum)
-                {
-                    foreach (string s in departaments[wtf[0]].Skip(3 * (int.Parse(wtf[1]))).Take(3).OrderBy(x => x)
-                        .ToArray())
+                    foreach (var patients in departments[line])
                     {
-                        Console.WriteLine(s);
-                    }
-                    return;
-                }
-                if (!isNum)
-                {
-                    foreach (KeyValuePair<string, List<string>> keyValuePair in doctors)
-                    {
-                        if (keyValuePair.Key == "ъ")
-                        {
-                            foreach (string s in keyValuePair.Value.OrderBy(x => x))
-                            {
-                                Console.WriteLine(s);
-                            }
-                        }
+                        Console.WriteLine(patients);
                     }
                 }
+                else if (int.TryParse(token[1], out int result))
+                {
+                    if (int.Parse(token[1]) > 20)
+                    {
+                        continue;
+                    }
+                    var patients = departments[token[0]];
+                    var room = patients.Skip( 3 * (int.Parse(token[1])-1)).Take(3).OrderBy(p=>p);
+                       
+                    foreach (var patient in room)
+                    {
+                        Console.WriteLine(patient);
+                    }
+                }
+                else
+                {
+                    var pat = doctors[line];
+                    pat.Sort();
+                    foreach (var patient in pat)
+                    {
+                        Console.WriteLine(patient);
+                       
+                    }    
+                }
+                line = Console.ReadLine();
             }
         }
     }
