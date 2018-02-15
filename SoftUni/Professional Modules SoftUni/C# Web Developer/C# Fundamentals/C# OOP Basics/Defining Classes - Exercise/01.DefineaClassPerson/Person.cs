7fef2491fc3b2cd922410d948e47bfdda007d584
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-15 21:52:50 +0200

    01.DefineaClassPerson

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Defining Classes - Exercise/01.DefineaClassPerson/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }    
+
+    //private string name;
+    //private int age;
+
+    //public string Name
+    //{
+    //    get { return name; }
+    //    set { name = value; }
+    //}
+    //public int Age
+    //{
+    //    get { return age; }
+    //    set { age = value; }
+    //}
+}
+
