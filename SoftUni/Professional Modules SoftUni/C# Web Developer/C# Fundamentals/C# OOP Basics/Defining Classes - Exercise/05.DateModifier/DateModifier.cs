3ef0c08246370a4060bdff36694101ef00a7decd
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-16 13:58:06 +0200

    05.DateModifier

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Defining Classes - Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+public class DateModifier
+{
+    public DateModifier(string startDate, string endDate)
+    {
+        DateTime start = DateTime.Parse(startDate);
+        DateTime end = DateTime.Parse(endDate);
+        var result = (start-end);
+        Console.WriteLine(Math.Abs(result.Days));
+
+    }
+}
---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Defining Classes - Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var startData = (Console.ReadLine());
+        var stopData = (Console.ReadLine());
+
+        var b= new DateModifier(startData,stopData);
+
+    }
+}
