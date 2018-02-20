0e7ca88024c81488cd07b61ac8b4ee393a9b070a
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-20 08:22:37 +0200

    12.Google

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Defining Classes - Exercise/12.Google/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿
+public class Pokemon
+{
+    private string name;
+    private string type;
+
+    public Pokemon(string name, string type)
+    {
+        this.name = name;
+        this.type = type;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.name} {this.type}";
+    }
+}
