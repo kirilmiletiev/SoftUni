cb6c5ad45e2a85b8547d1fcb5b9af0f8ba78594b
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-21 07:40:50 +0200

    2.PointInRectangle

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Working_with_Abstraction_Lab/2.PointInRectangle/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Rectangle
+{
+    private Point topLeft;
+
+    public Point TopLeft
+    {
+        get { return topLeft; }
+        set { topLeft = value; }
+    }
+
+
+    private Point bottomRight;
+
+    public Point BottomRight
+    {
+        get { return bottomRight; }
+        set { bottomRight = value; }
+    }
+
+    public Rectangle(Point topLeft, Point bottomRight)
+    {
+        this.TopLeft = topLeft;
+        this.BottomRight = bottomRight;
+    }
+
+
+    public bool Contains(Point point)
+    {
+        var contains = point.X >= TopLeft.X && point.X <= BottomRight.X &&
+                       point.Y >= topLeft.Y && point.Y <= BottomRight.Y;
+        Console.WriteLine(contains);
+        return contains;
+    }
+
+}
+
