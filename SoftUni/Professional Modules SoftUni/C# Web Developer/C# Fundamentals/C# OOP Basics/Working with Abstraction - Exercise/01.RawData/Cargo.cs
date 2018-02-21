58d398a7f7c108a067e71827d30bcb551d43d6a2
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-21 22:28:30 +0200

    Working with Abstraction - Exercise/01.RawData

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Working with Abstraction - Exercise/01.RawData/Cargo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Cargo
+{
+    public int cargoWeight;
+    public string Type;
+
+    public Cargo(int weight, string type)
+    {
+        this.cargoWeight = weight;
+        this.CargoType = type;
+    }
+
+    public string CargoType { get => Type; set => Type = value; }
+}
---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Working with Abstraction - Exercise/01.RawData/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Engine
+{
+    public int Speed;
+    public int Power;
+
+    public Engine(int speed, int power)
+    {
+        this.Speed = speed;
+        this.Power = power;
+    }
+}
