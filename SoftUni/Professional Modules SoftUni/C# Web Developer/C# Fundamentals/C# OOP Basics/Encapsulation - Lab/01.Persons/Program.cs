37b8f0faedcc924aaba85fff3fd058987e40811a
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-23 07:25:05 +0200

    01.Persons

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Encapsulation - Lab/01.Persons/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int lines = int.Parse(Console.ReadLine());
+        List<Person> persons = new List<Person>();
+
+        for (int i = 0; i < lines; i++)
+        {
+            var input = Console.ReadLine().Split();
+
+            var person = new Person(input[0], input[1], int.Parse(input[2]));
+            persons.Add(person);
+        }
+
+        foreach (Person person in persons.OrderBy(n=>n.FirstName).ThenBy(a=>a.Age))
+        {
+            Console.WriteLine(person);
+        }
+
+    }
+}
