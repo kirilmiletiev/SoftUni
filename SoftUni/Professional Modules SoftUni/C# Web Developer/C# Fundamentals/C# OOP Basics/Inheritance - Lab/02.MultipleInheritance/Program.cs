455216d0b1fd421bb1558b6e77892f18774f6fbc
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-26 07:59:07 +0200

    02.MultipleInheritance

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Inheritance - Lab/02.MultipleInheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02.MultipleInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
