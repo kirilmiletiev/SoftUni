daec6e2c62aa22c2123e8efd453f197e4c822dca
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-26 08:00:29 +0200

    04.RandomList

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Inheritance - Lab/04.RandomList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       // Random rnd = new Random();
+
+        RandomList rnd = new RandomList();
+        rnd.Add("pesho");
+        rnd.Add("gosho");
+        rnd.Add("Ivan");
+        rnd.Add("kiro");
+        
+        //rnd.RemoveRandomElemnt(rnd);
+
+    }
+}
