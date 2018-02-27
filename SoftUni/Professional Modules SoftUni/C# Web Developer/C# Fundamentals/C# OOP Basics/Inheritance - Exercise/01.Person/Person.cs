9f61cce671db8134d701955a32489f49974af3db
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-27 19:40:15 +0200

    01.Person

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Inheritance - Exercise/01.Person/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+    private string name;
+
+    public virtual string Name
+    {
+        get { return name; }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Name's length should not be less than 3 symbols!");
+            }
+            name = value;
+        }
+    }
+
+    private int age;
+
+    public virtual int Age
+    {
+        get { return age; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age must be positive!");
+            }
+            age = value;
+        }
+    }
+
+    //public Person()
+    //{
+
+    //}
+
+    public Person(string name, int age)
+    //:this()
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {this.Name}, Age: {this.Age}";
+    }
+}
