0af9c97fbac39c3eb753a77c9566615a92f7144a
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-02-28 12:00:38 +0200

    06.Animals

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Inheritance - Exercise/06.Animals/Animals/Kinds/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _06.Animals.Animals;
+
+public class Dog : Animal
+{
+    public Dog(string name, int age, string gender) : base(name, age, gender)
+    {
+    }
+
+    public override string ProduceSound()
+    {
+        return "Woof!";
+    }
+}
---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Inheritance - Exercise/06.Animals/Animals/Kinds/Kitten.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Kitten : Cat
+{
+    private const string Gender = "Female";
+
+    public Kitten(string name, int age) : base(name, age, Gender)
+    {
+    }
+
+    public override string ProduceSound()
+    {
+        return "Meow";
+    }
+}
