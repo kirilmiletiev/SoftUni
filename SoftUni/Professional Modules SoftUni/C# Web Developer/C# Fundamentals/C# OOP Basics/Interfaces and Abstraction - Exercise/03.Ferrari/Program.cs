10818af55684f8f114e7d2007df5b0fc4381613d
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-03-01 20:17:57 +0200

    03.Ferrari

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Interfaces and Abstraction - Exercise/03.Ferrari/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string name = Console.ReadLine();
+        Ferrari ferrari = new Ferrari(name);
+        string ferrariName = typeof(Ferrari).Name;
+        string iCarInterfaceName = typeof(ICar).Name;
+
+        bool isCreated = typeof(ICar).IsInterface;
+        if (!isCreated)
+        {
+            throw new Exception("No interface ICar was created");
+        }
+
+        Console.WriteLine(ferrari);
+    }
+}
