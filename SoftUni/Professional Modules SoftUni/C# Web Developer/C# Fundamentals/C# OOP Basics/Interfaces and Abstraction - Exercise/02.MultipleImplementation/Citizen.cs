cba8be6e4f7cb35a64cf379a26a39549eafdf02e
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-03-01 20:17:16 +0200

    02.MultipleImplementation

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Interfaces and Abstraction - Exercise/02.MultipleImplementation/Citizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Citizen : IBirthable, IIdentifiable, IPerson
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Birthdate { get; set; }
+    public string Id { get; set; }
+
+    public Citizen(string name, int age, string id, string birthdate)
+    {
+        Name = name;
+        Age = age;
+        this.Id = id;
+        this.Birthdate = birthdate;
+    }
+}
