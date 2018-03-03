df71516af29a0e7765e212378fc2fd580e53a6e9
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-03-03 20:36:00 +0200

    05.BorderControl

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Interfaces and Abstraction - Exercise/05.BorderControl/IControl.cs
@@ -0,0 +1,5 @@
+﻿
+public interface IControl
+{
+     string Id { get; set; }
+}
---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Interfaces and Abstraction - Exercise/05.BorderControl/Robot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Robot : IControl
+{
+    private string model;
+    private string id;
+
+    public Robot(string model, string id)
+    {
+        this.model = model;
+        this.id = id;
+    }
+
+    public string Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+}
