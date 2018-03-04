06bafbd78c45e176c52bac7dd0f5c4e8a66f08a5
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-03-04 23:02:12 +0200

    08. Military Elite

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Interfaces and Abstraction - Exercise/08.MilitaryElite/Commando.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Commando : SpecialisedSoldier, ICommando
+{
+    private IList<IMission> missions;
+
+    public Commando(string id, string firstName, string lastName, double salary, string corps, IEnumerable<IMission> missions)
+        : base(id, firstName, lastName, salary, corps)
+    {
+        this.missions = new List<IMission>(missions);
+    }
+
+    public IReadOnlyList<IMission> Missions
+    {
+        get
+        {
+            return this.missions as IReadOnlyList<IMission>;
+        }
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine(base.ToString())
+            .AppendLine("Missions:")
+            .Append("  ")
+            .Append(string.Join($"  {Environment.NewLine}", this.missions.Select(m => $"  {m.ToString()}")));
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Basics/Interfaces and Abstraction - Exercise/08.MilitaryElite/Mission.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Mission : IMission
+{
+    private readonly string[] possibleStates = new string[] { "inProgress", "Finished" };
+
+    private string codeName;
+    private string state;
+
+    public Mission(string codeName, string state)
+    {
+        this.codeName = codeName;
+        this.State = state;
+    }
+
+    public string CodeName { get; private set; }
+
+    public string State
+    {
+        get
+        {
+            return this.state;
+        }
+
+        set
+        {
+            if (!this.possibleStates.Contains(value))
+            {
+                throw new ArgumentException("Invalid mission state");
+            }
+
+            this.state = value;
+        }
+    }
+
+    public void CompleteMission()
+    {
+        this.state = "Finished";
+    }
+
+    public override string ToString()
+    {
+        return $"Code Name: {this.codeName} State: {this.State}";
+    }
+}
