1d176abe04b7950bf4e4fe601601c9f56a92f839
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-03-28 08:02:41 +0300

    LAb Generics

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Advanced/01.Box/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Box<T>
+{
+
+    public Box()
+    {
+        this.Stack = new Stack<T>();
+    }
+
+    private Stack<T> Stack;
+    
+
+    public void Add(T element)
+    {
+        Stack.Push(element);
+    }
+
+
+    public object Remove()
+    {
+        if (Stack.Count>0)
+        {
+           return  Stack.Pop();
+        }
+        throw new ArgumentException("Box is empty.");
+        
+    }
+
+    public int Count => Stack.Count;
+}
---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# Fundamentals/C# OOP Advanced/03.Scale/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _03.Scale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var test = new Scale<string>("1", "233232");
+            //Console.WriteLine(test.GetHeavier());
+        }
+    }
+}
