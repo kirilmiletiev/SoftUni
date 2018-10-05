577a243617630b7218dc0e4c9b7ba040e2b99990
kirilmiletiev <32306524+kirilmiletiev@users.noreply.github.com>
2018-10-05 23:33:12 +0300

    State Managment SIS SoftUni Web

---
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# WEB Development Basics/SIS HandMade HTTP SERVER- STATE MANAGEMENT/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,46 @@
+﻿namespace SIS.HTTP.Cookies
+{
+    using SIS.HTTP.Cookies.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        public HttpCookieCollection()
+        {
+            this.cookies = new Dictionary<string, HttpCookie>();
+        }
+
+        private readonly Dictionary<string, HttpCookie> cookies;
+
+
+        public void Add(HttpCookie cookie)
+        {
+            //this.cookies.Add(cookie.Key.ToString(), cookie);  // ??? is this work???
+            this.cookies[cookie.Key] = cookie;
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            return this.cookies.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            return this.cookies[key];
+        }
+
+        public bool HasCookies()
+        {
+            return cookies.Any();
+        }
+
+
+        public override string ToString()
+        {
+            return string.Join("; ", this.cookies.Values);
+        }
+    }
+}
