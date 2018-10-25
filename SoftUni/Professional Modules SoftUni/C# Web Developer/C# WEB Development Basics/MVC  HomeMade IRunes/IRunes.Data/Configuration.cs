273522f172b745ba5917821448bbbd3082c4b87d
Kiril Miletiev <kirilmiletiev@gmail.com>
2018-10-25 13:23:27 +0300

    Remove ConnectionString, and change color Track/Create.html

--- SoftUni/Professional Modules SoftUni/C# Web Developer/C# WEB Development Basics/MVC  HomeMade IRunes/IRunes.Data/Configuration.cs
+++ SoftUni/Professional Modules SoftUni/C# Web Developer/C# WEB Development Basics/MVC  HomeMade IRunes/IRunes.Data/Configuration.cs
@@ -7,6 +7,6 @@ namespace IRunes.Data
     public static class Configuration
     {
         public static readonly string ConnectionString =
-            @"Server=DESKTOP-8KI8MHA\SQLEXPRESS;Database=IRunes;Integrated Security=True;";
+            @"Server=.;Database=IRunes;Integrated Security=True;";
     }
 }
