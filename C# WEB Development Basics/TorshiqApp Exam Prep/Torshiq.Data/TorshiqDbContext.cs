e34ffaf5d3e34d8526456b2d5cafd76649136548
Kiril Miletiev <kirilmiletiev@gmail.com>
2018-10-30 06:49:22 +0200

    Create DbContext and add Migrations/20181029192427_Initial.cs

---
+++ C# WEB Development Basics/TorshiqApp Exam Prep/Torshiq.Data/TorshiqDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Torshiq.Models;
+
+namespace Torshiq.Data
+{
+    public class TorshiqDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<Report> Reports { get; set; }
+
+        public DbSet<TaskSector> TaskSectors { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=.;Database=Torshiq;Integrated Security=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+       
+    }
+}
