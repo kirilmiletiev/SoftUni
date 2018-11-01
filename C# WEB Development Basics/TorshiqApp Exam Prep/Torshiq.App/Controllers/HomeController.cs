7e4b0e4cf5255217f2562fdbfb6b64f3e5427c64
Kiril Miletiev <kirilmiletiev@gmail.com>
2018-11-01 06:30:43 +0200

    Some .html bugs fix and implement "/UserIndex" in HomeController.cs

--- C# WEB Development Basics/TorshiqApp Exam Prep/Torshiq.App/Controllers/HomeController.cs
+++ C# WEB Development Basics/TorshiqApp Exam Prep/Torshiq.App/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using SIS.Framework.ActionResults;
+using SIS.Framework.Attributes.Method;
+using Torshiq.App.ViewModel;
 
 namespace Torshiq.App.Controllers
 {
@@ -6,6 +8,19 @@ namespace Torshiq.App.Controllers
     {
         public IActionResult Index()
         {
+            if (this.Identity==null)
+            {
+                return this.View();
+            }
+
+            return this.View("/UserIndex");
+        }
+
+        [HttpGet]
+        public IActionResult Index(LoginViewModel model)
+        {
+
+
             return this.View();
         }
     }
