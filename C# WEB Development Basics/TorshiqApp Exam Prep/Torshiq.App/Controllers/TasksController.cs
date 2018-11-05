231d6b0e5eb09cc58d5044cf295848e45b076c58
Kiril Miletiev <kirilmiletiev@gmail.com>
2018-11-05 07:00:18 +0200

    Last from Torshiq.App ...for now..

---
+++ C# WEB Development Basics/TorshiqApp Exam Prep/Torshiq.App/Controllers/TasksController.cs
@@ -0,0 +1,31 @@
+﻿using SIS.Framework.ActionResults;
+using SIS.Framework.Attributes.Method;
+using Torshiq.App.Services.Contracts;
+using Torshiq.App.ViewModel;
+
+namespace Torshiq.App.Controllers
+{
+    public class TasksController : BaseController
+    {
+        private readonly ITaskService taskService;
+
+        public TasksController(ITaskService taskService)
+        {
+            this.taskService = taskService;
+        }
+
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(CreateViewModel model)
+        {
+            taskService.AddTask(model.Title, model.DueDate, model.Description, model.Participants);
+            return this.View();
+        }
+    }
+}
---
+++ C# WEB Development Basics/TorshiqApp Exam Prep/Torshiq.App/ViewModel/CreateViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Torshiq.Models;
+
+namespace Torshiq.App.ViewModel
+{
+    public class CreateViewModel
+    {
+        public CreateViewModel()
+        {
+            this.AffectedSectors = new HashSet<TaskSector>();
+        }
+
+        public string Title { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool IsReported { get; set; }
+
+        public string Description { get; set; }
+
+        public string Participants { get; set; }
+
+        public ICollection<TaskSector> AffectedSectors { get; set; }
+
+    }
+}
