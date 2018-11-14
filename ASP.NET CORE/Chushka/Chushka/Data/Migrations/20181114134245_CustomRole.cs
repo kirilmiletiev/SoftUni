f4245d378109eedc3ee0572277bcc3fe1b401e3d
Kiril Miletiev <kirilmiletiev@gmail.com>
2018-11-14 20:32:34 +0200

    Migrations

---
+++ ASP.NET CORE/Chushka/Chushka/Data/Migrations/20181114134245_CustomRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chushka.Data.Migrations
+{
+    public partial class CustomRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomRole",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomRole",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ ASP.NET CORE/Chushka/Chushka/Data/Migrations/20181114174304_IsLoggedIn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chushka.Data.Migrations
+{
+    public partial class IsLoggedIn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLoggedIn",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLoggedIn",
+                table: "AspNetUsers");
+        }
+    }
+}
