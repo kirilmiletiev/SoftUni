0d016f3276bcf3dd81fac48c96f1e55f865664d1
KirilMiletiev <kirilmiletiev@gmail.com>
2018-12-03 22:29:13 +0200

    Try to implement UserController.cs with scaffolding controller from UI. Also implement "add sender" functionality, but without button in layout.

---
+++ ASP.NET CORE/Suzy/Suzy/Controllers/UserController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Suzy.Data;
+using Suzy.Data.Models;
+
+namespace Suzy.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly SuzyDbContext _context;
+
+        public UserController(SuzyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Senders.ToListAsync());
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sender = await _context.Senders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sender == null)
+            {
+                return NotFound();
+            }
+
+            return View(sender);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Age,Address,PhoneNumber")] Sender sender)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sender);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sender);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sender = await _context.Senders.FindAsync(id);
+            if (sender == null)
+            {
+                return NotFound();
+            }
+            return View(sender);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Age,Address,PhoneNumber")] Sender sender)
+        {
+            if (id != sender.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sender);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SenderExists(sender.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sender);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sender = await _context.Senders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sender == null)
+            {
+                return NotFound();
+            }
+
+            return View(sender);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sender = await _context.Senders.FindAsync(id);
+            _context.Senders.Remove(sender);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SenderExists(int id)
+        {
+            return _context.Senders.Any(e => e.Id == id);
+        }
+    }
+}
