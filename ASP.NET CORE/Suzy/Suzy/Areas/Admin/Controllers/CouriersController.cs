01e5d16359ec864c186b6e09911d4d262e9a5eb9
KirilMiletiev <kirilmiletiev@gmail.com>
2018-12-04 12:08:25 +0200

    Test Admin area Scaffold implementation. (Without routs yet)

---
+++ ASP.NET CORE/Suzy/Suzy/Areas/Admin/Controllers/CouriersController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Suzy.Data;
+using Suzy.Data.Models;
+
+namespace Suzy.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouriersController : Controller
+    {
+        private readonly SuzyDbContext _context;
+
+        public CouriersController(SuzyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Couriers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Couriers.ToListAsync());
+        }
+
+        // GET: Admin/Couriers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courier = await _context.Couriers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (courier == null)
+            {
+                return NotFound();
+            }
+
+            return View(courier);
+        }
+
+        // GET: Admin/Couriers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Couriers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Age,PhoneNumber")] Courier courier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(courier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(courier);
+        }
+
+        // GET: Admin/Couriers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courier = await _context.Couriers.FindAsync(id);
+            if (courier == null)
+            {
+                return NotFound();
+            }
+            return View(courier);
+        }
+
+        // POST: Admin/Couriers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Age,PhoneNumber")] Courier courier)
+        {
+            if (id != courier.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(courier);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourierExists(courier.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(courier);
+        }
+
+        // GET: Admin/Couriers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var courier = await _context.Couriers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (courier == null)
+            {
+                return NotFound();
+            }
+
+            return View(courier);
+        }
+
+        // POST: Admin/Couriers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var courier = await _context.Couriers.FindAsync(id);
+            _context.Couriers.Remove(courier);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourierExists(int id)
+        {
+            return _context.Couriers.Any(e => e.Id == id);
+        }
+    }
+}
--- ASP.NET CORE/Suzy/Suzy/Startup.cs
+++ ASP.NET CORE/Suzy/Suzy/Startup.cs
@@ -84,6 +84,11 @@ namespace Suzy
 
             app.UseMvc(routes =>
             {
+
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
